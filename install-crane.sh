dbafe3ee1f222535b44bffd17c61dce8374597fc
Thomas Orozco <thomas@orozco.fr>
2014-08-22 13:12:24 +0200

    Update var names to avoid clashes

--- install-crane.sh
+++ install-crane.sh
@@ -7,15 +7,16 @@ set -o nounset
 # Parameters that can be overriden. Mainly just useful defaults.
 : ${CRANE_VERSION:="0.8.1"}
 : ${PLATFORM:="linux"}
-: ${ARCH:="amd64"}
+: ${CRANE_ARCH:="amd64"}
 
 # These probably shouldn't be changed
 : ${CRANE_BIN:="crane"}
 : ${CRANE_DIR:="/usr/local/bin"}
 
 # Checkinstall specific prameters
-: ${PACKAGE_MAINTAINER:="thomas@scalr.com"}
-: ${PACKAGE_DEPS:='lxc-docker \( \>= 1.0.0\)'}
+: ${CRANE_PKG_MAINTAINER:="thomas@scalr.com"}
+: ${CRANE_PKG_DEPS:='lxc-docker \( \>= 1.0.0\)'}
+: ${CRANE_PKG_LICENSE:='MIT'}
 
 
 # Actually install now
@@ -36,7 +37,7 @@ echo "Crane - Lift containers with ease" > description-pak
 
 # Actually install
 CRANE_REPO="https://github.com/michaelsauter/crane"
-curl -L -o "${CRANE_BIN}" "${CRANE_REPO}/releases/download/v${CRANE_VERSION}/crane_${PLATFORM}_${ARCH}"
+curl -L -o "${CRANE_BIN}" "${CRANE_REPO}/releases/download/v${CRANE_VERSION}/crane_${PLATFORM}_${CRANE_ARCH}"
 chmod +x -- "${CRANE_BIN}"
 checkinstall \
   --type=debian \
@@ -44,10 +45,10 @@ checkinstall \
   --pkgname="${CRANE_BIN}" \
   --pkgsource="${CRANE_REPO}" \
   --pkgversion="${CRANE_VERSION}" \
-  --pkgarch="${ARCH}" \
-  --pkglicense="MIT" \
-  --requires="${PACKAGE_DEPS}" \
-  --maintainer="${PACKAGE_MAINTAINER}" \
+  --pkgarch="${CRANE_ARCH}" \
+  --pkglicense="${CRANE_PKG_LICENSE}" \
+  --requires="${CRANE_PKG_DEPS}" \
+  --maintainer="${CRANE_PKG_MAINTAINER}" \
   --nodoc \
   --default \
   mv "${CRANE_BIN}" "${CRANE_DIR}"
